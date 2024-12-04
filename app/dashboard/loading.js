cc75c66321d9d9b2db6a3f9f435d13383f9f71be
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2024-12-04 20:07:46 +0600

    Dashboard loader

---
+++ app/dashboard/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div>
+      <h1 className="text-center font-bold text-2xl text-red-500">
+        Loading Dashboard...
+      </h1>
+    </div>
+  );
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,10 @@
-export default function PageDashboard() {
-  return;
+import { wait } from "@/lib/wait";
+
+export default async function PageDashboard() {
+  await wait();
+  return (
+    <>
+      <p className="text-center font-bold text-2xl">Dashboard Layout</p>
+    </>
+  );
 }
